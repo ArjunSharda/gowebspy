d5a41922f35aaaf06976897f0ac01e02971c20fc
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2025-03-10 07:26:56 -0500

    Update traceroute.go

--- pkg/gowebspy/traceroute.go
+++ pkg/gowebspy/traceroute.go
@@ -6,11 +6,12 @@ import (
 	"net"
 	"os/exec"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
-
 
 type TracerouteHop struct {
 	Number int
@@ -19,29 +20,25 @@ type TracerouteHop struct {
 	Host   string
 }
 
-
 func SimpleTraceroute(ctx context.Context, host string, maxHops int) ([]TracerouteHop, error) {
 	var hops []TracerouteHop
 	
-
 	nativeHops, err := nativeTraceroute(ctx, host, maxHops)
 	if err == nil {
 		return nativeHops, nil
 	}
 	
-
 	return externalTraceroute(ctx, host, maxHops)
 }
 
-
 func nativeTraceroute(ctx context.Context, host string, maxHops int) ([]TracerouteHop, error) {
 	var hops []TracerouteHop
+	
 	ips, err := net.LookupIP(host)
 	if err != nil || len(ips) == 0 {
 		return nil, fmt.Errorf("couldn't resolve host %s: %w", host, err)
 	}
 	
-
 	var targetIP net.IP
 	for _, ip := range ips {
 		if ip.To4() != nil {
@@ -50,18 +47,16 @@ func nativeTraceroute(ctx context.Context, host string, maxHops int) ([]Tracerou
 		}
 	}
 	
-
 	if targetIP == nil && len(ips) > 0 {
 		targetIP = ips[0]
 	}
 	
-
 	for ttl := 1; ttl <= maxHops; ttl++ {
 		hop := TracerouteHop{Number: ttl}
+		
 		hopCtx, cancel := context.WithTimeout(ctx, 2*time.Second)
 		defer cancel()
 		
-
 		dialer := net.Dialer{
 			Timeout: 1 * time.Second,
 			Control: func(network, address string, c syscall.RawConn) error {
@@ -84,13 +79,11 @@ func nativeTraceroute(ctx context.Context, host string, maxHops int) ([]Tracerou
 			conn.Close()
 		}
 		
-
 		hopIP := extractIPFromError(err)
 		if hopIP != "" {
 			hop.IP = hopIP
 			hop.RTT = elapsed
 			
-
 			hosts, err := net.LookupAddr(hopIP)
 			if err == nil && len(hosts) > 0 {
 				hop.Host = hosts[0]
@@ -111,7 +104,6 @@ func nativeTraceroute(ctx context.Context, host string, maxHops int) ([]Tracerou
 	return hops, nil
 }
 
-
 func extractIPFromError(err error) string {
 	if err == nil {
 		return ""
@@ -119,7 +111,6 @@ func extractIPFromError(err error) string {
 	
 	errMsg := err.Error()
 	
-
 	ipv4Regex := regexp.MustCompile(`\b(?:\d{1,3}\.){3}\d{1,3}\b`)
 	ipv4Matches := ipv4Regex.FindAllString(errMsg, -1)
 	
@@ -127,7 +118,6 @@ func extractIPFromError(err error) string {
 		return ipv4Matches[0]
 	}
 	
-
 	ipv6Regex := regexp.MustCompile(`([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}`)
 	ipv6Matches := ipv6Regex.FindAllString(errMsg, -1)
 	
@@ -141,7 +131,6 @@ func extractIPFromError(err error) string {
 func externalTraceroute(ctx context.Context, host string, maxHops int) ([]TracerouteHop, error) {
 	var cmd *exec.Cmd
 	
-
 	if isWindows() {
 		cmd = exec.CommandContext(ctx, "tracert", "-d", "-h", strconv.Itoa(maxHops), host)
 	} else {
@@ -153,7 +142,6 @@ func externalTraceroute(ctx context.Context, host string, maxHops int) ([]Tracer
 		return nil, fmt.Errorf("traceroute command failed: %w", err)
 	}
 	
-
 	return parseTracerouteOutput(string(output)), nil
 }
 
@@ -162,14 +150,12 @@ func parseTracerouteOutput(output string) []TracerouteHop {
 	
 	lines := strings.Split(output, "\n")
 	
-
 	for i := 1; i < len(lines); i++ {
 		line := strings.TrimSpace(lines[i])
 		if line == "" {
 			continue
 		}
 		
-
 		hop := parseLine(line)
 		if hop.Number > 0 {
 			hops = append(hops, hop)
@@ -179,18 +165,15 @@ func parseTracerouteOutput(output string) []TracerouteHop {
 	return hops
 }
 
-
 func parseLine(line string) TracerouteHop {
 	hop := TracerouteHop{}
 	
-
 	numRegex := regexp.MustCompile(`^\s*(\d+)`)
 	numMatch := numRegex.FindStringSubmatch(line)
 	if len(numMatch) > 1 {
 		hop.Number, _ = strconv.Atoi(numMatch[1])
 	}
 	
-
 	ipRegex := regexp.MustCompile(`\b(?:\d{1,3}\.){3}\d{1,3}\b|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}`)
 	ipMatch := ipRegex.FindString(line)
 	if ipMatch != "" {
@@ -199,7 +182,6 @@ func parseLine(line string) TracerouteHop {
 		hop.IP = "*"
 	}
 	
-
 	rttRegex := regexp.MustCompile(`(\d+(?:\.\d+)?) ms`)
 	rttMatch := rttRegex.FindStringSubmatch(line)
 	if len(rttMatch) > 1 {
@@ -210,13 +192,23 @@ func parseLine(line string) TracerouteHop {
 	return hop
 }
 
-
 func isWindows() bool {
 	return runtime.GOOS == "windows"
 }
 
-
-import (
-	"runtime"
-	"syscall"
-)
+func TracerouteIPv6(ctx context.Context, host string, maxHops int) ([]TracerouteHop, error) {
+	var cmd *exec.Cmd
+	
+	if isWindows() {
+		cmd = exec.CommandContext(ctx, "tracert", "-6", "-d", "-h", strconv.Itoa(maxHops), host)
+	} else {
+		cmd = exec.CommandContext(ctx, "traceroute6", "-n", "-m", strconv.Itoa(maxHops), host)
+	}
+	
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("IPv6 traceroute command failed: %w", err)
+	}
+	
+	return parseTracerouteOutput(string(output)), nil
+}
